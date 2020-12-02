72d590820a5108b7153cee8b02d0d652e566aecd
kylemcshea <kmcshea@stevens.edu>
2020-12-02 17:08:53 -0500

    added header and collapse animation

--- hangman-frontend/src/App.js
+++ hangman-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
-
+import Header from "./components/Header";
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "100vh",
@@ -15,6 +15,7 @@ export default function App() {
   return (
     <div className={classes.root}>
       <CssBaseline />;
+      <Header />
     </div>
   );
 }
---
+++ hangman-frontend/src/components/Header.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, IconButton, Toolbar, Collapse } from "@material-ui/core";
+import SortIcon from "@material-ui/icons/Sort";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  appbar: {
+    background: "none",
+    fontFamily: "Nunito",
+  },
+  appbarTitle: {
+    color: "#4d0aab",
+    flexGrow: "1",
+  },
+  appbarWrapper: {
+    width: "80%",
+    margin: "0 auto",
+  },
+  colorTitle: {
+    color: "#5AFF3D",
+  },
+  container: {
+    textAlign: "center",
+  },
+  icon: {
+    color: "#fff",
+    fontSize: "2rem",
+  },
+  title: {
+    color: "#4d0aab",
+    fontSize: "3rem",
+    fontFamily: "Nunito",
+  },
+  dropDown: {
+    color: "#4d0aab",
+    fontSize: "3rem",
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  });
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            Hangman <span className={classes.colorTitle}>Learning.</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br />
+            Hangman<span className={classes.colorTitle}>Learning!</span>
+          </h1>
+          <IconButton>
+            <ExpandMoreIcon className={classes.dropDown} />
+          </IconButton>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
