23366647f5fb0da93f5814c9957bc393b880e351
kylemcshea <kmcshea@stevens.edu>
2020-12-03 14:57:46 -0500

    Updates

---
+++ precache-manifest.225118620df69b6bb2e73943dfd3af12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a40b0e722a8e644841cacfb790a0e6a",
+    "url": "/Hangman_Learning_Bio/index.html"
+  },
+  {
+    "revision": "380db9fceab603a66241",
+    "url": "/Hangman_Learning_Bio/static/css/main.df66e512.chunk.css"
+  },
+  {
+    "revision": "c8c1201d3d28d165404d",
+    "url": "/Hangman_Learning_Bio/static/js/2.80ed2e21.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hangman_Learning_Bio/static/js/2.80ed2e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380db9fceab603a66241",
+    "url": "/Hangman_Learning_Bio/static/js/main.120da325.chunk.js"
+  },
+  {
+    "revision": "3a1bb36d4fb7d57feb60",
+    "url": "/Hangman_Learning_Bio/static/js/runtime-main.a0d89ea2.js"
+  }
+]);
